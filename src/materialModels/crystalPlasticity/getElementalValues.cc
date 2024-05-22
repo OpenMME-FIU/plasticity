ffcc239dfea52fcbf0acffa653e9716767b052c3
atallman <atallman@fiu.edu>
2024-05-22 15:07:28 -0400

    connecting F and T and P

--- src/materialModels/crystalPlasticity/getElementalValues.cc
+++ src/materialModels/crystalPlasticity/getElementalValues.cc
@@ -138,7 +138,7 @@ void crystalPlasticity<dim>::getElementalValues(FEValues<dim>& fe_values,
 				}
                 if (cellID+q == 0)
                 {
-                    this->pcout << "before plasticity\t" << P[0][0] <<"\t"<< P[1][0] <<"\t"<< P[2][0] <<"\n";
+                    this->pcout << "before plasticity P\t" << P[0][0] <<"\t"<< P[1][0] <<"\t"<< P[2][0] <<"\n";
                 }
 
 				//Update strain, stress, and tangent for current time step/quadrature point
@@ -146,7 +146,8 @@ void crystalPlasticity<dim>::getElementalValues(FEValues<dim>& fe_values,
 
                 if (cellID+q == 0)
                 {
-                    this->pcout << "after plasticity\t" << P[0][0] <<"\t"<< P[1][0] <<"\t"<< P[2][0]<<"\n";
+                    this->pcout << "after plasticity P\t" << P[0][0] <<"\t"<< P[1][0] <<"\t"<< P[2][0]<<"\n";
+                    this->pcout << "after plasticity T\t" << T[0][0] <<"\t"<< T[1][0] <<"\t"<< T[2][0]<<"\n";
                 }
 				//Fill local residual
 				for (unsigned int d=0; d<dofs_per_cell; ++d) {
