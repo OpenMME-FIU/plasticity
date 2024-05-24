a2cbf80bf1597e91e7c99d5964444f42f7204583
atallman <atallman@fiu.edu>
2024-05-24 13:45:02 -0400

    stateVar debug

--- src/materialModels/crystalPlasticity/calculatePlasticity.cc
+++ src/materialModels/crystalPlasticity/calculatePlasticity.cc
@@ -217,10 +217,12 @@ void crystalPlasticity<dim>::calculatePlasticity(unsigned int cellID,
               }
           }
       }
-
+    if (cellID+quadPtID == 0) {this->pcout << "statev #"<<nstatv_real<<"\t";}
       for (int i; i<nstatv_real;i++){
           stateVar_iter[cellID][quadPtID][i]=statev(i+1);
+          if (cellID+quadPtID == 0) {this->pcout << statev(i+1) <<" "<<stateVar_iter[cellID][quadPtID][i]<<" ";}
       }
+    if (cellID+quadPtID == 0) {this->pcout << "\n";}
       for(int i=0 ; i<dim ; i++){
           for(int j=0 ; j<dim ; j++){
               if(i == j)
